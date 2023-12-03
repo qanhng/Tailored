694e28f2ba1f2ddf5d5b4bde4a6ead10780c9c9b
Anh Nguyen <nguyen.anh16@northeastern.edu>
2023-12-03 10:56:47 -0500

    customers changes

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -35,3 +35,64 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+@customers.route('/wishlist', methods=['GET'])
+def get_wishlist(UserID):
+    cursor = db.get_db().cursor()
+    query= '''SELECT CI.ItemID, CI.Name AS ItemName, CI.Description, CI.Price, CI.Size, CI.BrandName, CI.StyleID
+            FROM Wishlist W
+            JOIN Clothing_Item CI ON W.WishlistID = CI.CartID
+            JOIN User U ON W.UserID = U.UserID
+            WHERE U.UserID = {0}'''.format(UserID)
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+
+@customers.route('/ShippingOptions', methods=['GET'])
+def get_shipping_option(UserID):
+    cursor = db.get_db().cursor()
+    query= '''SELECT 
+            SC.CartID,SC.Cost AS CartCost, SO.ShippingOptionID, SO.Cost AS ShippingCost, SO.Duration
+            FROM Shopping_Cart SC JOIN Shipping_Option SO ON SC.ShippingOptionID = SO.ShippingOptionID
+            JOIN User U ON SC.CartID = U.CartID
+            WHERE U.UserID = {0}'''.format(UserID)
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+@customers.route('/customers/PurchaseHistories/<PurchaseAesthetic>', methods=['GET'])
+def get_purchaseAesthetic(UserID):
+    query = 'SELECT PH.PurchaseAesthetic \
+            FROM Purchase_History PH JOIN User U ON PH.UserID = U.UserID \
+            JOIN Persona P ON U.UserID = P.UserID \
+            JOIN Style S ON P.StylePreference = S.AestheticName \
+            WHERE U.UserID = {0}'.format(UserID)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
