883ed82f9f5e0bb4950150a2ede267b1003632d9
Maya Prasad <prasad.ma@northeastern.edu>
2023-12-06 17:38:06 -0500

    last bit

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -202,37 +202,3 @@ def update_payment_method(userID):
     db.get_db().commit()
     return "successfully editted payment option for#{0}!".format(userid)
 
-# gets the shipping option duration for the customer
-@customers.route('/shippingOptions/<userID>', methods = ['GET'])
-def get_shipping_options(userID):
-    query = '''SELECT SO.Duration, SO.Cost, SO.ShippingOptionID, SI.Name, SI.City, SI.State, SI.Street, SI.ZipCode
-     FROM Shipping_Option SO  JOIN Shipping_Info SI on SO.ShippingOptionID = SI.ShippingOptionID
-        JOIN ShippingInfo_User SIU ON SI.ShippingInfoID = SIU.ShippingInfoID JOIN User U on SIU.UserID = U.UserID
-        WHERE U.UserID =  ''' + str(userID)
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-# gets all possible shipping options for all customers
-@customers.route('/allShippingOptions', methods = ['GET'])
-def get_all_shipping_options():
-    query = '''SELECT DISTINCT SO.Duration FROM Shipping_Option SO'''
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
