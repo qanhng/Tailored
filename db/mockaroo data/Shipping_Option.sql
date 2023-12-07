e85c874a6c750b5b1e5c2f19de670f5a63044552
Anh Nguyen <138420894+qanhng@users.noreply.github.com>
2023-12-06 22:13:50 -0500

    Shipping_Option.sql

---
+++ db/mockaroo data/Shipping_Option.sql
@@ -0,0 +1,7 @@
+insert into Shipping_Option (Cost, Duration) values (4.99, 'Standard Shipping - 5 business days');
+insert into Shipping_Option (Cost, Duration) values (9.99, 'Expedited Shipping - 2 business days');
+insert into Shipping_Option (Cost, Duration) values (14.99, 'Overnight Shipping');
+insert into Shipping_Option (Cost, Duration) values (8.99, 'Standard International Shipping - 7 business days');
+insert into Shipping_Option (Cost, Duration) values (20.99, 'Expedited International Shipping - 3 business days');
+insert into Shipping_Option (Cost, Duration) values (3.99, 'Expedited Shipping Selected Areas');
+insert into Shipping_Option (Cost, Duration) values (2.99, 'Standard Shipping Selected Areas');
